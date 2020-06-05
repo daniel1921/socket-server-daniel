2073d413f8f1f1fbd156ae694c24288ee09ca4ae
Daniel Mafla Bonet <danielmaflab@hotmail.com>
2020-06-05 00:39:30 -0500

    sección con sockets

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import  router  from "./routes/router";
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-const server = new Server();
+const server = Server.instance;
 
 // BODY PARSER
 server.app.use(bodyParser.urlencoded({extended:true}));
@@ -16,5 +16,5 @@ server.app.use(cors({origin: true, credentials: true}));
 server.app.use('/', router);
 
 server.start( () => {
-    console.log('Servidor corriendo en el puerto ' + server.port);
+    console.log('Servidor corriendo en el puerto: ' + server.port);
 })
